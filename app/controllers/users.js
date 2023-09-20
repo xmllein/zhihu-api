960584558ed391876ba95e56ee2a7b1ec0843ba5
lein <xmllein@163.com>
2023-09-20 13:59:06 +0800

    使用 koa-parameter 校验参数

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -8,26 +8,40 @@ class UserCtl {
 
   // 用户详情
   findById(ctx) {
-    if(ctx.params.id * 1 >= db.length) {
-        ctx.throw(412, '先决条件失败，id大于等于数组长度')
+    if (ctx.params.id * 1 >= db.length) {
+      ctx.throw(412, '先决条件失败，id大于等于数组长度')
     }
     ctx.body = db[ctx.params.id * 1]
   }
 
   // 创建用户
   create(ctx) {
+    ctx.verifyParams({
+      name: {type: 'string', required: true},
+      age: {type: 'number', required: false}
+    })
     db.push(ctx.request.body)
     ctx.body = ctx.request.body
   }
 
   // 更新用户
   update(ctx) {
+    if(ctx.params.id * 1 >= db.length) {
+        ctx.throw(412, '先决条件失败，id大于等于数组长度')
+    }
+    ctx.verifyParams({
+      name: {type: 'string', required: true},
+      age: {type: 'number', required: false}
+    })
     db[ctx.params.id * 1] = ctx.request.body
     ctx.body = ctx.request.body
   }
 
   // 删除用户
   delete(ctx) {
+    if(ctx.params.id * 1 >= db.length) {
+        ctx.throw(412, '先决条件失败，id大于等于数组长度')
+    }
     db.splice(ctx.params.id * 1, 1)
     ctx.status = 204
   }
--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const error = require('koa-json-error')
+const parameter = require('koa-parameter')
 const app = new Koa()
 // 路由入口
 const routing = require('./routes')
@@ -11,7 +12,11 @@ app.use(error({
     postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack, ...rest}
 }))
 
+// 解析参数
 app.use(bodyParser())
+// 校验参数
+app.use(parameter(app))
+// 路由
 routing(app)
 app.listen(3000, () => {
   console.log('server is running at http://localhost:3000')
