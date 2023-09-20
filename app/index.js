f68119cfd177f3e8883d1f032665e8cbdf412e5b
lein <xmllein@163.com>
2023-09-20 13:52:13 +0800

    使用 koa-json-error 处理错误

--- app/index.js
+++ app/index.js
@@ -1,20 +1,15 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
+const error = require('koa-json-error')
 const app = new Koa()
 // 路由入口
 const routing = require('./routes')
 
 // 错误处理中间件
-app.use(async (ctx, next) => {
-  try{
-    await next()
-  }catch (err){
-    ctx.status = err.status || err.statusCode || 500
-    ctx.body = {
-      message: err.message
-    }
-  }
-})
+// 生产环境下的错误处理（排除stack字段）
+app.use(error({
+    postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack, ...rest}
+}))
 
 app.use(bodyParser())
 routing(app)
