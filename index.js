1987824ea4e457fd31aece3ce09e57bdd82c9416
lein <xmllein@163.com>
2023-09-20 11:18:01 +0800

    完成中间件  Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const Koa = require('koa')
 const app = new Koa()
 
-app.use((ctx) => {
-  ctx.body = 'Hello World'
+app.use(async (ctx, next) => {
+  console.log(1)
+  next() // 下一个中间件
+    console.log(2)
+  ctx.body = 'Hello Zhihu API'
+
 })
+
+// 下一个中间件
+app.use(async (ctx, next) => {
+  console.log(3)
+  next()
+  console.log(4)
+})
+
+// 下一个中间件
+app.use(async (ctx, next) => {
+  console.log(5)
+})
+
+// 输出： 1 3 5 4 2
 
 app.listen(3000)
